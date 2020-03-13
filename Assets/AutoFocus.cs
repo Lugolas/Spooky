b8e2aa99aed8c0b3eaf9a0c3d57e9894d3b870a4
Lugolas <hugo.sallebert@orange.fr>
2020-03-13 11:07:04 +0100

    Itinial

---
+++ Assets/AutoFocus.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class AutoFocus : MonoBehaviour
+{
+  public PostProcessProfile profile;
+  private DepthOfField dof;
+  // Start is called before the first frame update
+  void Start()
+  {
+    profile.TryGetSettings<DepthOfField>(out dof);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    Ray rayMiddle = Camera.main.ScreenPointToRay(new Vector2(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2));
+    RaycastHit hitMiddle;
+
+    if (Physics.Raycast(rayMiddle, out hitMiddle, 5000))
+    {
+      if (dof != null)
+      {
+        dof.focusDistance.value = Vector3.Distance(hitMiddle.point, Camera.main.transform.position);
+        dof.enabled.value = true;
+      }
+    }
+  }
+}
